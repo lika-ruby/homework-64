630faad41c135f40512530335ae7f6bd914dd2e1
lika-ruby <mary.havrylova.2011@gmail.com>
2025-03-02 11:27:11 +0200

    Updates

--- index.a40a0799.js
+++
@@ -1,9 +0,0 @@
-const e=`
-  <ul class="product-list">
-    <li class="product-item">
-      <h2 class="name"></h2>
-      <p class="price">Price: </p>
-      <p class="description"></p>
-    </li>
-  </ul>`;console.log([{id:1,name:"Laptop",price:1500,description:"A high-performance laptop for all your needs."},{id:2,name:"Smartphone",price:700,description:"A modern smartphone with an excellent camera."},{id:3,name:"Headphones",price:200,description:"Noise-cancelling headphones for better focus."}]),console.log(e);
-//# sourceMappingURL=index.a40a0799.js.map
